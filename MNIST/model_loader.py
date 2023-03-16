afe58e05c5bf1150b4adbb622f897940b7eb8570
Maheep <maheep.s.brar@gmail.com>
2023-03-16 17:57:58 -0400

    Added files for MNIST models

---
+++ MNIST/model_loader.py
@@ -0,0 +1,21 @@
+import os
+import torch, torchvision
+import MNIST.models.dense as dense
+import MNIST.models.cnn as cnn
+import MNIST.models.vit as vit
+
+
+models = {
+        'dense_entropy' : dense.entropy,
+        'dense_sigent'  :
+        'dense_01'      : 
+        'dense_per'     :
+        'dense_log'     :
+        'dense_exp'     :
+        'dense_hinge'   :
+        'dense_ramp'    :
+        'dense_pinball' :
+        'dense_trupin'  :
+        'dense_rehinge' :
+
+        }
---
+++ MNIST/models/dense.py
@@ -0,0 +1,28 @@
+import torch
+
+
+
+class Dense(nn.Module):
+    def __init__(self, loss, optimizer):
+        super(Dense, self).__init__()
+        self.loss = loss
+        self.optimizezr = optimizer
+        # Define layers
+
+
+
+    def forward(self, x):
+
+
+
+
+
+
+
+
+
+
+
+
+def DenseEntropy():
+    return Dense()
