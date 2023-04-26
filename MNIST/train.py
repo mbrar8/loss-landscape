c053994c95d5e84e40e28c6cce853b4123e3b78d
Maheep Singh Brar <mbrar@scholar-fe05.rcac.purdue.edu>
2023-04-26 14:30:22 -0400

    might be bugged

--- MNIST/train.py
+++ MNIST/train.py
@@ -15,53 +15,6 @@ import torch.nn.parallel
 import model_loader
 import dataloader
 
-
-def train(trainloader, net, lossfxn, optimizer, use_cuda=True):
-    net.train()
-    train_loss = 0
-    correct = 0
-    total = 0
-
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
-        batch_size = inputs.size(0)
-        total += batch_size
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
-        optimizer.zero_grad()
-        inputs, targets = Variable(inputs), Variable(targets)
-        outputs = net(inputs)
-        loss = lossfxn(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()*batch_size
-        _, predicted = torch.max(outputs.data, 1)
-        correct += predicted.eq(targets.data).cpu().sum().item()
-
-
-    return train_loss/total, 100 - 100.*correct/total
-
-
-def test(testloader, net, lossfxn, use_cuda=True):
-    net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
-    
-    for batch_idx, (inputs, targets) in enumerate(testloader):
-        batch_size = inputs.size(0)
-        total += batch_size
-
-        if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda()
-        inputs, targets = Variable(inputs), Variable(targets)
-        outputs = net(inputs)
-        loss = lossfxn(outputs, targets)
-        test_loss += loss.item()*batch_size
-        _, predicted = torch.max(outputs.data, 1)
-        correct += predicted.eq(targets.data).cpu().sum().item()
-
-    return test_loss/total, 100 - 100.*correct/total
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', '-m', default='dense_entropy')
@@ -79,17 +32,15 @@ if __name__ == '__main__':
     if not os.path.exists('trained/' + save_folder):
         os.makedirs('trained/' + save_folder)
 
-    #f = open('trained/' + save_folder + '/log.out', 'a', 0)
-
     trainloader, testloader = dataloader.load_dataset()
 
-    net, lossfxn = model_loader.load(args.model)
+    model, lossfxn = model_loader.load(args.model)
     
-    if use_cuda:
-        net.cuda()
-        lossfxn = lossfxn.cuda()
+    #if use_cuda:
+    #    model.cuda()
+    #    lossfxn = lossfxn.cuda()
 
-    optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=0.0005)
+    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=0.0005)
 
     for epoch in range(1, 11):
         print('Epoch: ' + str(epoch))
@@ -128,4 +79,4 @@ if __name__ == '__main__':
             torch.save(model.state_dict(), model_path)
 
 
-	epoch += 1
+        epoch += 1
